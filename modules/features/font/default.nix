1a7a6a1fe93be70de96d0c7f3255692744f1d04e
Ephemeral <ephemeral.those316@slmails.com>
2026-06-26 12:07:34 +0600

    features/font: use font.packages option

--- modules/features/font/default.nix
+++ modules/features/font/default.nix
@@ -28,56 +28,60 @@ in
 
       config = lib.mkIf cfg.enable {
         environment.pathsToLink = toList "/share/fonts";
-        packages = with pkgs; [
-          electroharmonix
-          google-fonts
-          fontconfig
-          material-symbols
-          nerd-fonts.jetbrains-mono
-          noto-fonts
-          noto-fonts-cjk-sans
-          noto-fonts-cjk-serif
-          noto-fonts-color-emoji
-          roboto
-          roboto-flex
-          roboto-mono
-          roboto-serif
-          roboto-slab
-          twemoji-color-font
-        ];
+        fonts = {
+          enableDefaultPackages = false;
+          packages = with pkgs; [
+            electroharmonix
+            google-fonts
+            material-symbols
+            nerd-fonts.jetbrains-mono
+            noto-fonts
+            noto-fonts-cjk-sans
+            noto-fonts-cjk-serif
+            noto-fonts-color-emoji
+            roboto
+            roboto-flex
+            roboto-mono
+            roboto-serif
+            roboto-slab
+            twemoji-color-font
+          ];
+          fontconfig = {
+            inherit (cfg) enable;
+            antialias = true;
+            hinting.enable = true;
+            allowBitmaps = true;
+            useEmbeddedBitmaps = false;
+            cache32Bit = true;
+            defaultFonts = {
+              sansSerif = unique [
+                cfg.sans
+                "Rubik"
+                "Roboto"
+                "Noto Sans"
+                "Noto Sans Bengali"
+              ];
 
-        fonts.fontconfig = {
-          inherit (cfg) enable;
-          antialias = true;
-          hinting.enable = true;
-          defaultFonts = {
-            sansSerif = unique [
-              cfg.sans
-              "Rubik"
-              "Roboto"
-              "Noto Sans"
-              "Noto Sans Bengali"
-            ];
+              monospace = unique [
+                cfg.mono
+                "JetBrainsMono Nerd Font"
+                "Roboto Mono"
+                "monospace"
+              ];
 
-            monospace = unique [
-              cfg.mono
-              "JetBrainsMono Nerd Font"
-              "Roboto Mono"
-              "monospace"
-            ];
+              serif = unique [
+                cfg.serif
+                "Roboto Serif"
+                "Noto Serif"
+                "Noto Serif Bengali"
+              ];
 
-            serif = unique [
-              cfg.serif
-              "Roboto Serif"
-              "Noto Serif"
-              "Noto Serif Bengali"
-            ];
-
-            emoji = [
-              "Twitter Color Emoji"
-              "Noto Color Emoji"
-              "Twemoji"
-            ];
+              emoji = [
+                "Twitter Color Emoji"
+                "Noto Color Emoji"
+                "Twemoji"
+              ];
+            };
           };
         };
       };
