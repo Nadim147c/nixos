1c8842f20081831f28dc80dfb301c8ab7fa9f9ce
Ephemeral <ephemeral.those316@slmails.com>
2026-06-26 12:07:34 +0600

    programs/kdeconnect: add kdeconnect-indicator services

--- modules/programs/kdeconnect/default.nix
+++ modules/programs/kdeconnect/default.nix
@@ -1,5 +1,24 @@
+{ lib, ... }:
+let
+  inherit (lib) toList getExe';
+in
 {
-  flake.modules.nixos.pc = {
+  flake.modules.nixos.pc = { pkgs, ... }: {
     programs.kdeconnect.enable = true;
+
+    home.systemd.services.kdeconnect = rec {
+      enable = true;
+      description = "Kdeconnect Indicator";
+      requires = toList "tray.target";
+      partOf = toList "graphical-session.target";
+      after = [
+        "graphical-session.target"
+        "tray.target"
+      ];
+      wantedBy = partOf;
+      serviceConfig = {
+        ExecStart = getExe' pkgs.kdePackages.kdeconnect-kde "kdeconnect-indicator";
+      };
+    };
   };
 }
