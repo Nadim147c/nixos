5b3cdfec2a4e1f4ca75ede86dba99b49bff5884a
Ephemeral <ephemeral.those316@slmails.com>
2026-06-26 12:07:34 +0600

    programs/kopuz: use (early) nixpkgs version

--- flake.nix
+++ flake.nix
@@ -64,6 +64,8 @@
       url = "github:Nadim147c/yankd";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    kopuz.url = "github:NixOS/nixpkgs/e33da78d58d221d54809ea02369b6d652a48a04e";
   };
 
   outputs =
--- modules/programs/kopuz/default.nix
+++ modules/programs/kopuz/default.nix
@@ -1,48 +1,19 @@
 {
+  inputs,
   self,
-  lib,
   ...
 }:
-let
-  inherit (lib) toList escapeShellArg;
-  appId = "com.temidaradev.kopuz";
-  sha256 = "sha256-Xbwb8Cpa2dUsR0viPHxz870H+ENfscevFTd8zkcTHFc=";
-  url = "https://github.com/Kopuz-org/kopuz/releases/download/v0.7.0/kopuz.flatpak";
-in
 {
-  perSystem =
-    { pkgs, ... }:
-    {
-      packages.kopuz = pkgs.writeShellScriptBin "kopuz" /* bash */ ''
-        exec -a "kopuz" flatpak run -- ${escapeShellArg appId} "$@"
-      '';
-    };
-  flake.modules.nixos.gui =
-    {
-      config,
-      pkgs,
-      system,
-      ...
-    }:
-    {
-      services.flatpak = {
-        packages = toList {
-          inherit sha256 appId;
-          bundle = toString <| pkgs.fetchurl { inherit sha256 url; };
-        };
-        overrides.settings."${appId}" = {
-          Context.filesystems = [
-            "xdg-config/kopuz"
-            "xdg-music"
-            "xdg-run/app/${config.programs.discord.appId}"
-          ];
-        };
-      };
-      packages = [
-        self.packages.${system}.kopuz
-        pkgs.picard
-        pkgs.nicotine-plus
-        pkgs.lrcget
-      ];
-    };
+  perSystem = { system, ... }: {
+    packages.kopuz = (import inputs.kopuz { inherit system; }).kopuz;
+  };
+
+  flake.modules.nixos.gui = { pkgs, system, ... }: {
+    packages = [
+      self.packages.${system}.kopuz
+      pkgs.picard
+      pkgs.nicotine-plus
+      pkgs.lrcget
+    ];
+  };
 }
