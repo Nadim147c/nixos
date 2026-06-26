86ebfe6d627306739489ec0c2f38e62cf6024543
Ephemeral <ephemeral.those316@slmails.com>
2026-06-26 12:07:34 +0600

    features/impurity-tracker: init

---
+++ modules/features/impurity-tracker/default.nix
@@ -0,0 +1,19 @@
+# Copied from https://github.com/NotAShelf/nyx/blob/d407b4d6e5ab7f60350af61a3d73a62a5e9ac660/modules/core/common/system/security/impurity-tracker.nix
+{
+  flake.modules.nixos.base =
+    { config, pkgs, ... }:
+    let
+      makeTracker =
+        name: target: exe:
+        pkgs.writeShellScript name ''
+          echo "PID $PPID executed ${target}" |& ${config.systemd.package}/bin/systemd-cat --identifier=impurity >/dev/null 2>/dev/null
+          exec -a "$0" '${exe}' "$@"
+        '';
+    in
+    {
+      environment = {
+        usrbinenv = makeTracker "env" "/usr/bin/env" "${pkgs.coreutils}/bin/env";
+        binsh = makeTracker "sh" "/bin/sh" "${pkgs.bashInteractive}/bin/sh";
+      };
+    };
+}
