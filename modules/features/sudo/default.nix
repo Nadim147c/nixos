ca147f3f4930f386a2311869b7b378455ca14850
Ephemeral <ephemeral.those316@slmails.com>
2026-06-26 12:07:34 +0600

    features/sudo: init

---
+++ modules/features/sudo/default.nix
@@ -0,0 +1,15 @@
+{ lib, ... }:
+let
+  inherit (lib.modules) mkForce;
+in
+{
+  flake.modules.nixos.base = {
+    security = {
+      sudo = {
+        enable = true;
+        wheelNeedsPassword = mkForce true;
+        execWheelOnly = mkForce true;
+      };
+    };
+  };
+}
